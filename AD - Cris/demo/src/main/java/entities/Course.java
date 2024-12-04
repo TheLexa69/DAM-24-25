48601e3ceaad15819e3e44f7077c0efce7d81a56
a20luisgag <guille1insua@gmail.com>
2024-12-04 12:57:08 +0100

    demo

---
+++ AD - Cris/demo/src/main/java/entities/Course.java
@@ -0,0 +1,43 @@
+package entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "course", schema = "hogwarts")
+public class Course {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    private entities.Person teacher;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public entities.Person getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(entities.Person teacher) {
+        this.teacher = teacher;
+    }
+
+}
